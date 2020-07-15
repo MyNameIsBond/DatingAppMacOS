af2c3e31b8c6c91cd7b5a2b561c5562b317de2f9
johns351 <johns351@coventry.ac.uk>
2020-07-15 21:33:51 +0100

    Initial Commit

---
+++ DatingAppMacOS/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DatingAppMacOS
+//
+//  Created by Tony Hajdini on 15/07/2020.
+//  Copyright © 2020 Tony Hajdini. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
